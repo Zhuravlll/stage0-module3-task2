cd69895f36e32558a68ea538750184d74e29162f
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-04-07 14:37:24 +0300

    Module #3, task #2 corrected naming issues

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,6 +1,6 @@
 package lang.print.gaps.task2;
 
-public class AdvancedNamingConventio {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
     private int age;
     private int phoneNumber;
